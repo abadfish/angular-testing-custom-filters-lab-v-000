7e79686acb17b91b84cef4baba78c4b0f16d6a9f
abadfish <kirsten.ofarrell@gmail.com>
2016-11-25 14:34:49 -0500

    done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,21 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter on favorite food', function() {
+		var mockedList = [{
+			name: 'Tim',
+			favoriteFood: 'pizza'
+		}, {
+			name: 'Roger',
+			favoriteFood: 'lettuce'
+		}, {
+			name: 'Ellen',
+			favoriteFood: 'meatballs'
+		}];
+
+		var results = $filter('favoriteFood')(mockedList, 'lettuce');
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Roger');
+
+	});
 });
